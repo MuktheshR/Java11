a7333a3f7cefd9b4c45943256c2ed1ac80634af6
MuktheshR <98938244+MuktheshR@users.noreply.github.com>
2022-03-02 11:15:41 +0530

    Add Java 11 Assignment

---
+++ JAVA11/Java11q3.java
@@ -0,0 +1,19 @@
+package JAVA11;
+
+
+import java.util.ArrayList;
+
+public class Java11Q3 {
+// In this we are  converting a array list to a string  by toArray method
+	public static void main(String[] args) {
+		ArrayList<String>list=new ArrayList<>();
+         list.add("A quick brown fox jumps over the lazy dog");
+         System.out.println(list);  
+         System.out.println("------------");  
+//         String[]arr = list.toArray(new String[list.size()]);
+         String[]arr = list.toArray(size->new String[size]);
+         for(String j:arr){
+          System.out.println(j);
+	}
+	}
+}
